7241c21bca43809188c8119c6b2ba2d449f50857
satishgowda <satish@keubik.com>
2020-02-19 07:25:22 +0530

    cart dropdown populated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.style.scss';
 
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItem}) => (
   <div className='cart-dropdown'>
     <div className="cart-items">
-
+      {
+        cartItem.map((item) => <CartItem key={item.id} item={item}/>)
+      }
     </div>
     <CustomButton>Go To Checkout</CustomButton>
   </div>
 );
-
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItem}}) => ({
+  cartItem
+})
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './cart-item.style.scss';
+
+const CartITem = ({item: {name, imageUrl, price, quantity}}) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item'/>
+    <div className='item-details'>
+      <span className='name'>{name}</span>
+      <span className='price'>{quantity} X ${price}</span>
+    </div>
+  </div>
+);
+
+export default CartITem;
